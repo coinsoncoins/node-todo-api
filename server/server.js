2a3f2dce931d92e6e4b6cbf16f2bd243696dd990
Coins On Coins <coinsoncoins00@gmail.com>
2017-11-28 13:50:39 -0600

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ let bodyParser = require('body-parser');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 let app = express();
 app.use(bodyParser.json());
@@ -25,6 +26,26 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  })
+  console.log(req.body);
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+    console.log('ID was invalid');
+    return;
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+      console.log('ID not found');
+    } else {
+      res.send({todo});
+    }
+  }, (e) => {
+    res.status(400).send()
   })
   console.log(req.body);
 })
