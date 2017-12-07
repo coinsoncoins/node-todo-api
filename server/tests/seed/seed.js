97168c93b193f5c66b160bf6238775865f962991
Coins On Coins <coinsoncoins00@gmail.com>
2017-12-07 12:51:39 -0600

    use secret file for env variables

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -11,7 +11,7 @@ const users = [{
   password: "password1",
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: user1Id, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: user1Id, access: 'auth'}, process.env.JWT_SECRET).toString()
   }],
 }, {
   _id: user2Id,
@@ -19,7 +19,7 @@ const users = [{
   password: "password2",
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: user2Id, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: user2Id, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }]
 
